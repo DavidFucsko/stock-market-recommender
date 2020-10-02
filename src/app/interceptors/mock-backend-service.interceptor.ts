5539e1899b0a83c3d7b8264e7d4dc918b7841da0
DavidFucsko <szocsyke2@gmail.com>
2020-10-02 19:53:00 +0200

    Add backend functionality

--- src/app/interceptors/mock-backend-service.interceptor.ts
+++ src/app/interceptors/mock-backend-service.interceptor.ts
@@ -9,23 +9,118 @@ import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+
 import { environment } from 'src/environments/environment';
+import { CountByType } from '../models/count-by-type.model';
+import { SocialMediaCount } from '../models/social-media-count.model';
+import { StockPriceModel } from '../models/stock-price.model';
+
 
 @Injectable()
 export class MockBackendServiceInterceptor implements HttpInterceptor {
+    private algorithmMap: Map<string, (...args) => number> = new Map(
+        [['naive', this.naiveAlgorithm], ['advanced', this.advancedAlgorithm]]
+    );
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (environment.backendEndpoints.includes(request.url.split(environment.backendUrl)[1])) {
-            console.log(`Intercepted: ${request.url}`);
-            return of(new HttpResponse({ body: 'success', status: 200 }));
+        switch (request.url.split(environment.backendUrl)[1]) {
+            case 'stockPriceGenerator': {
+                const stockSymbolParam = request.params.get('stockSymbol');
+                return (of(new HttpResponse({
+                    body:
+                        this.stockPriceGenerator(
+                            stockSymbolParam,
+                            new Date(request.params.get('fromDate')),
+                            new Date(request.params.get('toDate'))
+                        ), status: 200
+                })));
+            }
+            case 'socialMediaCountGenerator': {
+                const stockSymbol = request.params.get('stockSymbol');
+                return (of(new HttpResponse({
+                    body: this.socialMediaCountGenerator(
+                        stockSymbol,
+                        new Date(request.params.get('fromDate')),
+                        new Date(request.params.get('toDate'))
+                    ), status: 200
+                })));
+            }
+            case 'recommendationAlgorithm':
+                {
+                    const stockPrice: number[] = request.body['stockPrices'];
+                    const socialMediaCount: number[] = request.body['socialMediaCounts'];
+                    const algorithmToUse: string = request.body['algorithmToUse'];
+                    const additionalParams: any[] = request.body['additionalParams'];
+                    return of(new HttpResponse({
+                        body: this.recommendationAlgorithm(
+                            stockPrice,
+                            socialMediaCount,
+                            algorithmToUse,
+                            additionalParams
+                        ), status: 200
+                    }));
+                }
+            default:
+                return next.handle(request).pipe(
+                    map((event: HttpEvent<any>) => {
+                        if (event instanceof HttpResponse) {
+                            console.log('event--->>>', event);
+                        }
+                        return event;
+                    }));
         }
-        else {
-            return next.handle(request).pipe(
-                map((event: HttpEvent<any>) => {
-                    if (event instanceof HttpResponse) {
-                        console.log('event--->>>', event);
-                    }
-                    return event;
-                }));
+    }
+
+    stockPriceGenerator(stockSymbol: string, fromDate: Date, toDate: Date): StockPriceModel[] {
+        const response: StockPriceModel[] = [];
+        const dayOfTrade = fromDate;
+        while (dayOfTrade.getTime() < toDate.getTime()) {
+            response.push({ stockSymbol, price: (Math.random() * 1001), dayOfTrade });
+            dayOfTrade.setDate(dayOfTrade.getDate() + 1);
         }
+        return response;
+    }
+
+    socialMediaCountGenerator(stockSymbol: string, fromDate: Date, toDate: Date): SocialMediaCount[] {
+        const response: SocialMediaCount[] = [];
+        const dayOfTrade = fromDate;
+        while (dayOfTrade.getTime() < toDate.getTime()) {
+            const socialMediaCountsForDay: CountByType[] = [
+                {
+                    mediaType: 'facebook',
+                    count: (Math.random() * 10001)
+                },
+                {
+                    mediaType: 'twitter',
+                    count: (Math.random() * 10001)
+                },
+                {
+                    mediaType: 'instagram',
+                    count: (Math.random() * 10001)
+                }
+            ];
+            response.push({ stockSymbol, socialMediaCounts: socialMediaCountsForDay, dayOfTrade });
+            dayOfTrade.setDate(dayOfTrade.getDate() + 1);
+        }
+
+        return response;
+    }
+
+    recommendationAlgorithm(stockPrices: number[], socialMediaCounts: number[], algorithmToUse: string, ...additionalParams): number[] {
+        let result: number[] = [];
+        result = stockPrices.map((stockPrice, index) => {
+            return this.algorithmMap.get(algorithmToUse).call(this, stockPrice, socialMediaCounts[index], additionalParams[index]);
+        });
+        return result;
+    }
+
+    private naiveAlgorithm(stockPrice: number, socialMediaCount: number): number {
+        // if stock price higher than social media count per 1000 we recommend sell, if equal we recommend hold,
+        // else buy it
+        return stockPrice > socialMediaCount / 1000 ? 2 : stockPrice === socialMediaCount / 1000 ? 1 : 0;
+    }
+
+    private advancedAlgorithm(stockPrice: number, socialMediaCount: number, constantFactor: number): number {
+        return stockPrice * constantFactor > socialMediaCount ? 2 : stockPrice * constantFactor === socialMediaCount ? 1 : 0;
     }
 }
--- src/app/services/stock-rating/stock-rating.service.ts
+++ src/app/services/stock-rating/stock-rating.service.ts
@@ -1,10 +1,120 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { StockRatingViewModule } from '../../views/stock-rating-view/stock-rating-view.module';
+import { forkJoin, Observable, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { SearchStockParamters } from 'src/app/models/search-stock-parameters.model';
+import { SocialMediaCount } from 'src/app/models/social-media-count.model';
+import { SocialPlatforms } from 'src/app/models/social-platforms.model';
+import { StockPriceModel } from 'src/app/models/stock-price.model';
+import { StockSearchResult } from 'src/app/models/stock-search-results.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: StockRatingViewModule
+  providedIn: 'root'
 })
 export class StockRatingService {
 
-  constructor() { }
+  private latestCombinedSearchResults: StockSearchResult;
+
+  constructor(private httpClient: HttpClient) { }
+
+  stockPriceGenerator(searchParamters: SearchStockParamters): Observable<StockPriceModel[]> {
+    let stockPriceGeneratorParams = new HttpParams();
+    stockPriceGeneratorParams = stockPriceGeneratorParams.append(
+      'stockSymbol', searchParamters.stockSymbol);
+    stockPriceGeneratorParams = stockPriceGeneratorParams.append(
+      'fromDate', new Date(searchParamters.fromDate).toISOString().substring(0, 10));
+    stockPriceGeneratorParams = stockPriceGeneratorParams.append(
+      'toDate', new Date(searchParamters.toDate).toISOString().substring(0, 10));
+
+
+    return this.httpClient.get<StockPriceModel[]>(
+      `${environment.backendUrl}${environment.backendEndpoints[0]}`,
+      { params: stockPriceGeneratorParams }).pipe(
+        catchError(_ => of([])));
+  }
+
+  socialMediaCountGenerator(searchParamters: SearchStockParamters): Observable<SocialMediaCount[]> {
+    let socialMediaCountParams = new HttpParams();
+    socialMediaCountParams = socialMediaCountParams.append(
+      'stockSymbol', searchParamters.stockSymbol);
+    socialMediaCountParams = socialMediaCountParams.append(
+      'fromDate', new Date(searchParamters.fromDate).toISOString().substring(0, 10));
+    socialMediaCountParams = socialMediaCountParams.append(
+      'toDate', new Date(searchParamters.toDate).toISOString().substring(0, 10));
+
+    return this.httpClient.get<SocialMediaCount[]>(
+      `${environment.backendUrl}${environment.backendEndpoints[1]}`,
+      { params: socialMediaCountParams }).pipe(
+        catchError(_ => of([])));
+  }
+
+  recommendationAlgorithm(
+    stockPrices: number[],
+    socialMediaCounts: number[],
+    algorithmToUse: string = 'naive',
+    ...additionalParams): Observable<number[]> {
+
+    const body = { stockPrices, socialMediaCounts, algorithmToUse, additionalParams };
+
+    return this.httpClient.post<number[]>(`${environment.backendUrl}${environment.backendEndpoints[2]}`, body);
+  }
+
+  searchStock(searchParameters: SearchStockParamters): Observable<StockSearchResult> {
+    return forkJoin({
+      stockPrices: this.stockPriceGenerator(searchParameters),
+      socialMediaCounts: this.socialMediaCountGenerator(searchParameters)
+    }).pipe(
+      map(result => {
+        const combinedResults: StockSearchResult = new StockSearchResult();
+        const combinedStockPrices = [];
+        const combinedDayOfTrades = [];
+        const combinedSocialMediaCounts: number[] = this.filterBySocialMediaPlatforms(result.socialMediaCounts, searchParameters);
+        result.stockPrices.forEach((stockPrice) => {
+          combinedStockPrices.push(stockPrice.price);
+          combinedDayOfTrades.push(stockPrice.dayOfTrade);
+        });
+        combinedResults.cumulatedSocialMediaCounts = combinedSocialMediaCounts;
+        combinedResults.stockPrices = combinedStockPrices;
+        combinedResults.dayOfTrades = combinedDayOfTrades;
+        combinedResults.socialMediaCounts = result.socialMediaCounts;
+        this.latestCombinedSearchResults = combinedResults;
+        return combinedResults;
+      }),
+      switchMap(
+        combinedResults => this.recommendationAlgorithm(
+          combinedResults.stockPrices,
+          combinedResults.cumulatedSocialMediaCounts, searchParameters.algorithmToUse, searchParameters.additionalParams).pipe(
+            map(response => ({ ...combinedResults, ...{ stockRatings: response } })))),
+    );
+  }
+
+  runRecommendationAlgorithm(searcParams: SearchStockParamters): Observable<StockSearchResult> {
+
+    const socialMediaParamteres = { facebook: searcParams.facebook, twitter: searcParams.twitter, instagram: searcParams.instagram };
+    if (!!this.latestCombinedSearchResults) {
+      const filteredSocialMediaCounts = this.filterBySocialMediaPlatforms(
+        this.latestCombinedSearchResults.socialMediaCounts, socialMediaParamteres);
+      return this.recommendationAlgorithm(
+        this.latestCombinedSearchResults.stockPrices,
+        filteredSocialMediaCounts,
+        searcParams.algorithmToUse, searcParams.additionalParams).pipe(
+          map(response => (
+            { ...this.latestCombinedSearchResults, ...{ stockRatings: response, cumulatedSocialMediaCounts: filteredSocialMediaCounts } }))
+        );
+    }
+
+    return of(new StockSearchResult());
+  }
+
+  private filterBySocialMediaPlatforms(socialMediaCounts: SocialMediaCount[], socialMediaParamteres: SocialPlatforms): number[] {
+    const combinedSocialMediaCounts = [];
+    socialMediaCounts.forEach(socialMediaCount => {
+      combinedSocialMediaCounts.push(socialMediaCount.socialMediaCounts
+        .filter(countByType => socialMediaParamteres[countByType.mediaType])
+        .reduce((previous, countByType) => (previous += countByType.count), 0));
+    });
+
+    return combinedSocialMediaCounts;
+  }
 }
